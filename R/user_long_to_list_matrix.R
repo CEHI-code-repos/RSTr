4c9d24b4efd7eeefe679b3c0013cb5d4b2fecd08
sfq-1 <sfq1@cdc.gov>
2025-12-11 09:10:43 -0500

    renamed generate_data() to long_to_list_matrix()

--- R/user_long_to_list_matrix.R
+++ R/user_long_to_list_matrix.R
@@ -1,8 +1,8 @@
 #' Generate count data for RSTr object
 #' 
-#' \code{generate_data()} converts a long \code{table} featuring event counts across regions and other optional margins into a \code{list} that is readable by \code{*car()}.
+#' \code{long_to_list_matrix()} converts a long \code{table} featuring event counts across regions and other optional margins into a \code{list} that is readable by \code{*car()}.
 #' 
-#' \code{generate_data()} will sum along any group/time stratifications that aren't specified; for example, if your dataset contains time periods and time is not specified in \code{generate_data()}, the output will be a sum of all time periods. Filter data by desired groups and time periods before running \code{generate_data()}.
+#' \code{long_to_list_matrix()} will sum along any group/time stratifications that aren't specified; for example, if your dataset contains time periods and time is not specified in \code{long_to_list_matrix()}, the output will be a sum of all time periods. Filter data by desired groups and time periods before running \code{long_to_list_matrix()}.
 #' 
 #' @param table A \code{table} containing event and mortality counts stratified by group/region/time.
 #' @param event The column containing event counts.
@@ -14,14 +14,14 @@
 #' @examples
 #' ma_data <- maexample[!is.na(maexample$Year), ]
 #' # Generates data from 1979-1981 stratified by sex
-#' ma_data_mst <- generate_data(ma_data, Deaths, Population, County.Code, Sex.Code, Year.Code)
+#' ma_data_mst <- long_to_list_matrix(ma_data, Deaths, Population, County.Code, Sex.Code, Year.Code)
 #' ma_data_79 <- ma_data[ma_data$Year == 1979, ]
 #' # Generates 1979 data stratified by sex
-#' ma_data_m <- generate_data(ma_data_79, Deaths, Population, County.Code, Sex.Code)
+#' ma_data_m <- long_to_list_matrix(ma_data_79, Deaths, Population, County.Code, Sex.Code)
 #' # Generates 1979 data summarized for all sexes
-#' ma_data_u <- generate_data(ma_data_79, Deaths, Population, County.Code)
+#' ma_data_u <- long_to_list_matrix(ma_data_79, Deaths, Population, County.Code)
 #' @export
-generate_data <- function(table, event, population, region, group = NULL, time = NULL) {
+long_to_list_matrix <- function(table, event, population, region, group = NULL, time = NULL) {
   ev <- deparse(substitute(event))
   po <- deparse(substitute(population))
   re <- deparse(substitute(region))
