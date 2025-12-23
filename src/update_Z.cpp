49f58ba05af41ebd6424c0d01179be72b5f9eda5
sfq-1 <sfq1@cdc.gov>
2025-12-23 09:19:25 -0500

    improve update_Z

--- src/update_Z.cpp
+++ src/update_Z.cpp
@@ -1,6 +1,8 @@
 #include <RcppArmadillo.h>
 #include <RcppDist.h>
 #include "cpp_helpers.h"
+using namespace arma;
+using namespace Rcpp;
 using arma::vec;
 using arma::mat;
 using arma::cube;
@@ -9,18 +11,70 @@ using arma::field;
 using arma::uvec;
 using Rcpp::List;
 
-
-mat get_mean_Z(const cube& Z, const cube& lambda, const cube& beta, 
-               const mat& tau2, const mat& sig2, const field<uvec>& adjacency,
-               const uvec& isl_id, const mat& var_Z, const uword reg) {
+mat get_mean_Z_car(const cube& Z, const cube& lambda, const cube& beta, 
+                   const mat& tau2, const mat& sig2, const field<uvec>& adjacency,
+                   const uvec& isl_id, const mat& var_Z, const uword reg) {
   mat sum_adj = arma::sum(get_regs(Z, adjacency[reg]));
-  mat rate_diff = lambda.row(reg) - beta.row(isl_id[reg]);
+  mat lmb_i = lambda.row(reg) - beta.row(isl_id[reg]);
   if (Z.n_slices == 1) {
     sum_adj = sum_adj.t();
-    rate_diff = rate_diff.t();
+    lmb_i = lmb_i.t();
   }
-  const mat mean_Z = var_Z % (rate_diff / tau2 + sum_adj / sig2);
+  const mat mean_Z = var_Z % (lmb_i / tau2 + sum_adj / sig2);
   return mean_Z;
+}
+
+inline vec get_lmb_it(const cube& lambda, const cube& beta, const uvec& isl_id,
+                      const uword time, const uword reg) {
+  return (get_grp(lambda, reg, time) - get_grp(beta, isl_id[reg], time));
+}
+
+inline mat get_nZm(const cube& Z, const uvec& regs) {
+  const uword n_time = Z.n_slices;
+  mat nZm = mean(get_regs(Z, regs), 0);
+  if (n_time == 1) {
+    return nZm.t();
+  }
+  return nZm;
+}
+
+inline vec get_muZp(const mat& nZm, const field<mat>& Se, const cube& Z,
+                    const uword reg, const uword time) {
+  const uword n_time = Z.n_slices;
+  vec muZp = nZm.col(time);
+  if (time > 0) {
+   muZp += Se(time, time - 1) * (nZm.col(time - 1) - get_grp(Z, reg, time - 1));
+  }
+  if (time < n_time - 1) {
+    muZp += Se(time, time + 1) * (nZm.col(time + 1) - get_grp(Z, reg, time + 1));
+  }
+  return muZp;
+}
+
+field<mat> get_cov_Z_mstcar(const field<mat>& Sein, const vec& tau2, 
+                            const vec& n_adj) {
+  const uword n_time = Sein.n_rows;
+  field<mat> cov_Z(n_time, max(n_adj) + 1);
+  const vec unique_n_adj = unique(n_adj);
+  for (uword time = 0; time < n_time; ++time) {
+    for (uword count : unique_n_adj) {
+      const mat it2_diag = diagmat(1.0 / tau2);
+      cov_Z(time, count) = inv_sympd(it2_diag + count * Sein(time, time));
+    }
+  }
+  return cov_Z;
+}
+
+field<mat> get_coveig_Z_mstcar(const field<mat>& cov_Z, const vec& n_adj) {
+  const uword n_time = cov_Z.n_rows;
+  field<mat> coveig_Z(n_time, max(n_adj) + 1);
+  const vec unique_n_adj = unique(n_adj);
+  for (uword time = 0; time < n_time; ++time) {
+    for (uword count : unique_n_adj) {
+      coveig_Z(time, count) = geteig(cov_Z(time, count));
+    }
+  }
+  return coveig_Z;
 }
 
 void demean_Z(cube& Z, const field<uvec>& isl_region, const uvec& isl_id) {
@@ -32,6 +86,34 @@ void demean_Z(cube& Z, const field<uvec>& isl_region, const uvec& isl_id) {
     Zkt.row(isl) = arma::mean(get_regs(Z, isl_region[isl]), 0);
   }
   Z -= get_regs(Zkt, isl_id);
+}
+
+field<mat> get_cov_Z_mcar(const mat& tau2, const mat& G, const vec& n_adj) {
+  const vec unique_n_adj = unique(n_adj);
+  field<mat> cov_Z(max(n_adj) + 1);
+  for (uword count : unique_n_adj) {
+    const mat it2_diag = diagmat(1.0 / tau2);
+    cov_Z(count) = inv_sympd(it2_diag + count * G);
+  }
+  return cov_Z;
+}
+
+field<mat> get_coveig_Z_mcar(const field<mat>& cov_Z, const vec& n_adj) {
+  const vec unique_n_adj = unique(n_adj);
+  field<mat> coveig_Z(max(n_adj) + 1);
+  for (uword count : unique_n_adj) {
+    coveig_Z(count) = geteig(cov_Z(count));
+  }
+  return coveig_Z;
+}
+
+void replace_Zit(cube& Z, const vec& mean_Z, const mat& cov_Z, const uword reg,
+                 const uword time) {
+  const uword n_group = Z.n_cols;
+  vec Z_new = cpp_rmvnorm(mean_Z, cov_Z);
+  for (uword grp = 0; grp < n_group; ++grp) {
+    Z(reg, grp, time) = Z_new(grp);
+  }
 }
 
 //[[Rcpp::export]]
@@ -51,8 +133,8 @@ void update_Z_car(List& RSTr_obj) {
   
   for (uword reg = 0; reg < n_region; ++reg) {
     const mat var_Z = 1.0 / (1.0 / tau2 + n_adj[reg] / sig2);
-    const mat mean_Z = get_mean_Z(Z, lambda, beta, tau2, sig2, adjacency, 
-                                  isl_id, var_Z, reg);
+    const mat mean_Z = get_mean_Z_car(Z, lambda, beta, tau2, sig2, adjacency, 
+                                      isl_id, var_Z, reg);
     Z.row(reg) = rnorm_mat(mean_Z, sqrt(var_Z));
   }
   demean_Z(Z, isl_region, isl_id);
@@ -65,36 +147,29 @@ void update_Z_car(List& RSTr_obj) {
 void update_Z_mcar(List& RSTr_obj) {
   List sample = RSTr_obj["sample"];
   cube Z = sample["Z"];
-  cube G = sample["G"];
-  cube lambda = sample["lambda"];
-  cube beta = sample["beta"];
-  mat tau2 = sample["tau2"];
-  List sp_data = RSTr_obj["sp_data"];
-  field<uvec> adjacency = sp_data["adjacency"];
-  vec n_adj = sp_data["n_adj"];
-  field<uvec> isl_region = sp_data["isl_region"];
-  uvec isl_id = sp_data["isl_id"];
-  uword n_region = Z.n_rows;
-  uword n_group = Z.n_cols;
-  uword n_time = Z.n_slices;
-  field<mat> Z_cov(max(n_adj) + 1, n_time);
-  field<mat> Z_coveig(max(n_adj) + 1, n_time);
-  cube rate_diff = lambda - get_regs(beta, isl_id);
-  vec unique_n_adj = unique(n_adj);
+  const cube& G = sample["G"];
+  const cube& lambda = sample["lambda"];
+  const cube& beta = sample["beta"];
+  const mat& tau2 = sample["tau2"];
+  const List& sp_data = RSTr_obj["sp_data"];
+  const field<uvec>& adjacency = sp_data["adjacency"];
+  const vec& n_adj = sp_data["n_adj"];
+  const field<uvec>& isl_region = sp_data["isl_region"];
+  const uvec& isl_id = sp_data["isl_id"];
+  const uword n_region = Z.n_rows;
+  const uword n_time = Z.n_slices;
+
+  const cube& lmb = lambda - get_regs(beta, isl_id);
   for (uword time = 0; time < n_time; ++time) {
-    vec taut = tau2.col(time);
-    mat Gt = G.slice(time);
-    for (uword count : unique_n_adj) {
-      Z_cov(count, time) = inv_sympd(diagmat(1.0 / taut) + count * Gt);
-      Z_coveig(count, time) = geteig(Z_cov(count, time));
-    }
+    const vec& tau2t = tau2.col(time);
+    const mat& Gt = G.slice(time);
+    const field<mat> cov_Z = get_cov_Z_mcar(tau2t, Gt, n_adj);
+    const field<mat> coveig_Z = get_coveig_Z_mcar(cov_Z, n_adj);
     for (uword reg = 0; reg < n_region; ++reg) {
-      vec sum_adj = sum(get_subgrp(Z, adjacency[reg], time), 0).t();
-      vec Z_mean = Z_cov(n_adj[reg], time) * (get_grp(rate_diff, reg, time) / taut + Gt * sum_adj);
-      vec Z_new  = cpp_rmvnorm(Z_mean, Z_coveig(n_adj[reg], time));
-      for (uword grp = 0; grp < n_group; ++grp) {
-        Z(reg, grp, time) = Z_new(grp);
-      }
+      const vec lmb_it = get_lmb_it(lambda, beta, isl_id, time, reg);
+      const vec sum_adj = sum(get_subgrp(Z, adjacency[reg], time), 0).t();
+      const vec mean_Z = cov_Z(n_adj[reg]) * (lmb_it / tau2t + Gt * sum_adj);
+      replace_Zit(Z, mean_Z, coveig_Z(n_adj[reg]), reg, time);
     }
   }
   demean_Z(Z, isl_region, isl_id);
@@ -107,47 +182,31 @@ void update_Z_mcar(List& RSTr_obj) {
 void update_Z_mstcar(List& RSTr_obj) {
   List sample = RSTr_obj["sample"];
   cube Z = sample["Z"];
-  cube G = sample["G"];
-  cube lambda = sample["lambda"];
-  cube beta = sample["beta"];
-  vec rho = sample["rho"];
-  vec tau2 = sample["tau2"];
-  List sp_data = RSTr_obj["sp_data"];
-  field<uvec> adjacency = sp_data["adjacency"];
-  vec n_adj = sp_data["n_adj"];
-  field<uvec> isl_region = sp_data["isl_region"];
-  uvec isl_id = sp_data["isl_id"];
-  uword n_region  = Z.n_rows;
-  uword n_group   = Z.n_cols;
-  uword n_time    = Z.n_slices;
-  field<mat> Sein   = Sig_eta_i(G, rho);
-  field<mat> SeSein = Sig_eta(Sein);
-  field<mat> Z_cov(n_time, max(n_adj) + 1);
-  field<mat> Z_coveig(n_time, max(n_adj) + 1);
-  vec unique_n_adj = unique(n_adj);
-  for (uword time = 0; time < n_time; ++time) {
-    for (uword count : unique_n_adj) {
-      Z_cov   (time, count) = inv_sympd(diagmat(1.0 / tau2) + count * Sein(time, time));
-      Z_coveig(time, count) = geteig(Z_cov(time, count));
-    }
-  }
-  cube rate_diff = lambda - get_regs(beta, isl_id);
+  const cube& G = sample["G"];
+  const cube& lambda = sample["lambda"];
+  const cube& beta = sample["beta"];
+  const vec& rho = sample["rho"];
+  const vec& tau2 = sample["tau2"];
+  const List& sp_data = RSTr_obj["sp_data"];
+  const field<uvec>& adjacency = sp_data["adjacency"];
+  const vec& n_adj = sp_data["n_adj"];
+  const field<uvec>& isl_region = sp_data["isl_region"];
+  const uvec& isl_id = sp_data["isl_id"];
+  const uword n_region = Z.n_rows;
+  const uword n_time = Z.n_slices;
+
+  const field<mat> Sein = Sig_eta_i(G, rho);
+  const field<mat> Se = Sig_eta(Sein);
+  const field<mat> cov_Z = get_cov_Z_mstcar(Sein, tau2, n_adj);
+  const field<mat> coveig_Z = get_coveig_Z_mstcar(cov_Z, n_adj);
   for (uword reg = 0; reg < n_region; ++reg) {
-    mat nZm = mean(get_regs(Z, adjacency[reg]), 0);
-    if (n_time == 1) nZm = nZm.t();
+    const mat nZm = get_nZm(Z, adjacency[reg]);
     for (uword time = 0; time < n_time; ++time) {
-      vec muZp = nZm.col(time);
-      if (time > 0) {
-        muZp += SeSein(time, time - 1) * (nZm.col(time - 1) - get_grp(Z, reg, time - 1));
-      }
-      if (time < n_time - 1) {
-        muZp += SeSein(time, time + 1) * (nZm.col(time + 1) - get_grp(Z, reg, time + 1));
-      }
-      mat Z_mean = Z_cov(time, n_adj[reg]) * (get_grp(rate_diff, reg, time) / tau2 + (n_adj[reg] * Sein(time, time) * muZp));
-      vec Z_new  = cpp_rmvnorm(Z_mean, Z_coveig(time, n_adj[reg]));
-      for (uword grp = 0; grp < n_group; ++grp) {
-        Z(reg, grp, time) = Z_new(grp);
-      }
+      const vec lmb_it = get_lmb_it(lambda, beta, isl_id, time, reg);
+      const vec muZp = get_muZp(nZm, Se, Z, reg, time);
+      const vec agg_Z = lmb_it / tau2 + (n_adj[reg] * Sein(time, time) * muZp);
+      const vec mean_Z = cov_Z(time, n_adj[reg]) * agg_Z;
+      replace_Zit(Z, mean_Z, coveig_Z(time, n_adj[reg]), reg, time);
     }
   }
   demean_Z(Z, isl_region, isl_id);
