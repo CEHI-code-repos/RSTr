2b9ee746d5b0fc9d6496d83e4ccaf22ab001c8c5
sfq-1 <sfq1@cdc.gov>
2026-01-15 12:35:03 -0500

    create split_sample_group()

---
+++ R/user_split_sample_groups.R
@@ -0,0 +1,41 @@
+#' Split sample groups
+#'
+#' Sequesters stratified sociodemographic group margin into individual array margins.
+#'
+#' When using \code{aggregate_samples()} or \code{standardize_samples()}, the group/age margin must only feature groups of similar type, e.g., you cannot age-standardize with groups that specify both age and race. \code{split_sample_groups()} sequesters each category of group into its own margin to allow group-aggregation and age-standardization of these multiply-stratified groups. Ensure that the delimiter character is only used to split groups. E.g., for an age-sex group named \code{35-64_m}, \code{"_"} will split the margins with names \code{"35-64"} and \code{"m"}, whereas for a group named \code{35_64_m}, \code{split_sample_group()} will fail.
+#'
+#' @param sample an \code{array} of samples imported with \code{load_samples()}
+#' @param new_groups A string vector of names for each new group.
+#' @param delimiter A character that specifies the break between group categories.
+#' @returns An \code{array} of samples with separate margins for stratified groups.
+#' @examples
+#' dimnames(minsplit)[2] # Can't age-standardize due to age-sex stratification
+#' new_groups = c("age", "sex")
+#' delimiter = "_"
+#' sample_split <- split_sample_groups(minsplit, new_groups, delimiter)
+#' dimnames(sample_split)[2:3] # can now age-standardize
+#' std_pop <- c(113154, 100640, 95799)
+#' age_margin <- 2
+#' sample_as <- standardize_samples(sample_split, std_pop, age_margin)
+#' @export
+split_sample_groups <- function(samples, new_groups, delimiter = "_") {
+  ng <- length(new_groups)
+  group_dims <- dimnames(samples)[[2]] |>
+    strsplit(delimiter) |>
+    simplify2array() |>
+    apply(1, unique) |>
+    setNames(new_groups) |>
+    rev()
+  samples_new_dims <- c(
+    dimnames(samples)[1],
+    group_dims,
+    dimnames(samples)[3:4]
+  )
+  sample_new <- array(
+    samples,
+    dim = sapply(samples_new_dims, length),
+    dimnames = samples_new_dims
+  ) |>
+    aperm(c(1, 2 + rev(0:(ng - 1)), ng + 2:3))
+  sample_new
+}
