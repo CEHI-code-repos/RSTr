20a749ff8e2a6fce2c5a6ccd4827eff8b1dc40d7
David DeLara <sfq1@cdc.gov>
2026-01-24 21:33:20 -0500

    fixed update_Z for memory safety

--- src/update_Z.cpp
+++ src/update_Z.cpp
@@ -190,6 +190,7 @@ void update_Z_mcar(List& RSTr_obj) {
 void update_Z_mstcar(List& RSTr_obj) {
   List sample = RSTr_obj["sample"];
   cube Z = sample["Z"];
+  //auto Z = Rcpp::as<cube>(sample["Z"]);
   const auto G = Rcpp::as<cube>(sample["G"]);
   const auto lambda = Rcpp::as<cube>(sample["lambda"]);
   const auto beta = Rcpp::as<cube>(sample["beta"]);
