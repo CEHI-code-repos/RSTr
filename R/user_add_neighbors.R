f6aa4c04546df65f905f48261fd743e71bf3e761
sfq-1 <sfq1@cdc.gov>
2026-01-28 17:41:19 -0500

    add requireNamespace for sf

--- R/user_add_neighbors.R
+++ R/user_add_neighbors.R
@@ -8,21 +8,23 @@
 #' @param neighs A vector of regions to mark as adjacent. Accepts a vector of indices or names assigned to \code{adjacency}.
 #' @returns A modified \code{adjacency} \code{list}.
 #' @examples
-#' sf::sf_use_s2(FALSE)
+#' if (requireNamespace("sf", quietly = TRUE)) {
+#'   sf::sf_use_s2(FALSE)
 #'
-#' mamap <- sf::st_as_sf(mamap[order(mamap$GEOID), ])
-#' ma_adj <- spdep::poly2nb(mamap)
-#' new_neighs <- c(1, 4, 10) # attach regions 1, 4, and 10
-#' ma_adj <- add_neighbors(ma_adj, new_neighs)
+#'   mamap <- sf::st_as_sf(mamap[order(mamap$GEOID), ])
+#'   ma_adj <- spdep::poly2nb(mamap)
+#'   new_neighs <- c(1, 4, 10) # attach regions 1, 4, and 10
+#'   ma_adj <- add_neighbors(ma_adj, new_neighs)
 #'
-#' # Add neighbors by FIPS code instead of index
-#' ma_adj <- suppressWarnings(spdep::poly2nb(mamap))
-#' names(ma_adj) <- mamap$GEOID
-#' ma_adj <- add_neighbors(ma_adj, neighs = c("25001", "25007", "25019"))
+#'   # Add neighbors by FIPS code instead of index
+#'   ma_adj <- suppressWarnings(spdep::poly2nb(mamap))
+#'   names(ma_adj) <- mamap$GEOID
+#'   ma_adj <- add_neighbors(ma_adj, neighs = c("25001", "25007", "25019"))
 #'
-#' ma_adj <- suppressWarnings(spdep::poly2nb(mamap))
-#' ma_adj <- add_neighbors(ma_adj, c(1, 4)) # only attach 1 and 4
-#' ma_adj <- add_neighbors(ma_adj, c(4, 10)) # only attach 4 and 10
+#'   ma_adj <- suppressWarnings(spdep::poly2nb(mamap))
+#'   ma_adj <- add_neighbors(ma_adj, c(1, 4)) # only attach 1 and 4
+#'   ma_adj <- add_neighbors(ma_adj, c(4, 10)) # only attach 4 and 10
+#' }
 #' @export
 add_neighbors <- function(adjacency, neighs) {
   if (is.character(neighs)) {
