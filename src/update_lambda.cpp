8ff7583afa115d6046501406369e4578ac5ef004
David DeLara <sfq1@cdc.gov>
2026-01-28 20:35:09 -0500

    switch auto calls back to cubes

--- src/update_lambda.cpp
+++ src/update_lambda.cpp
@@ -35,20 +35,20 @@ inline double get_r(const double l_0, const double l_star, const double b_0,
 //[[Rcpp::export]]
 void update_lambda(List& RSTr_obj) {
   List sample = RSTr_obj["sample"];
-  auto lambda = Rcpp::as<cube>(sample["lambda"]);
-  const auto Z = Rcpp::as<cube>(sample["Z"]);
-  const auto beta = Rcpp::as<cube>(sample["beta"]);
-  const auto tau2 = Rcpp::as<mat>(sample["tau2"]);
+  cube lambda = Rcpp::as<cube>(sample["lambda"]);
+  const cube Z = Rcpp::as<cube>(sample["Z"]);
+  const cube beta = Rcpp::as<cube>(sample["beta"]);
+  const mat tau2 = Rcpp::as<mat>(sample["tau2"]);
   const List& data = RSTr_obj["data"];
-  const auto Y = Rcpp::as<cube>(data["Y"]);
-  const auto n = Rcpp::as<cube>(data["n"]);
+  const cube Y = Rcpp::as<cube>(data["Y"]);
+  const cube n = Rcpp::as<cube>(data["n"]);
   List priors = RSTr_obj["priors"];
-  auto lambda_acpt = Rcpp::as<cube>(priors["lambda_acpt"]);
-  const auto lambda_sd = Rcpp::as<cube>(priors["lambda_sd"]);
+  cube lambda_acpt = Rcpp::as<cube>(priors["lambda_acpt"]);
+  const cube lambda_sd = Rcpp::as<cube>(priors["lambda_sd"]);
   const List& sp_data = RSTr_obj["sp_data"];
-  const auto isl_id = Rcpp::as<uvec>(sp_data["isl_id"]);
+  const uvec isl_id = Rcpp::as<uvec>(sp_data["isl_id"]);
   const List& params = RSTr_obj["params"];
-  const auto method = Rcpp::as<string>(params["method"]);
+  const string method = Rcpp::as<string>(params["method"]);
   const uword n_region = Z.n_rows;
   const uword n_group = Z.n_cols;
   const uword n_time = Z.n_slices;
